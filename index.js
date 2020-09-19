bc3a423ee8e52466d8576aeb106f1f525aa1e1db
Ariel K <ar.kogan@gmail.com>
2020-09-19 16:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function iterativeLog(array) {
   })
   }
 
+  function callback(arr) {
+    return arr
+  }
+
   function iterate(callback) {
     var letters = ['a', 'b', 'c']
     letters.forEach(callback)
